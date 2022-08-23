914ce986ebdf7938adf9b2d98338ab2dc1e20ed0
Vladimir Stepanov <vladimir-stepanov-home@yandex.ru>
2022-08-23 16:05:08 +0300

    Добавлена AlarmActivity, реализован её запуск при срабатывании будильника.

--- app/src/main/java/com/template/alarmmanagerproject/AlertReceiver.java
+++ app/src/main/java/com/template/alarmmanagerproject/AlertReceiver.java
@@ -9,13 +9,20 @@ public class AlertReceiver extends BroadcastReceiver {
 
     /**
      * Этот метод будет вызван, когда сработает будильник
-     * @param context
-     * @param intent
+     *
+     * @param context С помощью контекста мы можем создавать интенты и другие действия
+     * @param intent Этот интент можно проверять, прежде чем выполнять обработку
+     *               события: например, из него можно вытащить строку и обработать
+     *               её дальше
      */
     @Override
     public void onReceive(Context context, Intent intent) {
         NotificationHelper notificationHelper = new NotificationHelper(context);
         Notification.Builder nb = notificationHelper.getChannelNotification();
         notificationHelper.getManager().notify(1, nb.build());
+
+        Intent myIntent = new Intent(context, AlarmActivity.class);
+        myIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(myIntent);
     }
 }
